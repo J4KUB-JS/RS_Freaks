f257d12a0e3a07b236b0e99ac07241aa4f7b951d
J4KUB-JS <jakubspirydonjs@gmail.com>
2022-10-25 19:34:55 +0200

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./app/css/style.css";
+import "./style.css";
 import { store } from "./app/redux/store";
 import { Provider } from "react-redux";
 
